92420b2a5e4399df2d93364878df1bfb764dd848
AlexandrTolstov <alextav_85@mail.ru>
2019-01-28 21:50:27 +0600

    1

--- ThreadingMonitor/Program.cs
+++ ThreadingMonitor/Program.cs
@@ -2,14 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ThreadingMonitor
 {
     class Program
     {
+        static int x = 0;
+        static object locker = new object();
         static void Main(string[] args)
         {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread myThread = new Thread(Count);
+                myThread.Name = "Поток " + i.ToString();
+                myThread.Start();
+            }
+            Console.ReadLine();
+        }
+
+        private static void Count()
+        {
+            try
+            {
+                Monitor.Enter(locker);
+                x = 1;
+                for (int i = 0; i < 9; i++)
+                {
+                    Console.WriteLine("{0}: {1}", Thread.CurrentThread.Name, x);
+                    x++;
+                    Thread.Sleep(100);
+                }
+            }
+            finally
+            {
+                Monitor.Exit(locker);
+            }
         }
     }
 }
